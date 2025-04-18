7649d6c38882fd3ffdd4afef3b387c3fa9c1b043
sebpb <sebastianperezbertoli@gmail.com>
2025-04-18 18:21:39 +0000

    hacer: etapa 1 y 2

--- client/main.go
+++ client/main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"client/globals"
 	"client/utils"
+	"log"
 )
 
 func main() {
 	utils.ConfigurarLogger()
 
 	// loggear "Hola soy un log" usando la biblioteca log
-	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
-	// validar que la config este cargada correctamente
+	log.Println("Hola soy un log")
 
+	// validar que la config este cargada correctamente
+	globals.ClientConfig = utils.IniciarConfiguracion("config.json")
+	if globals.ClientConfig == nil {
+		log.Fatalf("No se pudo cargar la configuración")
+	}
 	// loggeamos el valor de la config
+	log.Printf(globals.ClientConfig.Mensaje)
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"server/utils"
 )
@@ -8,10 +9,11 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
+	log.Println("Servidor corriendo en el puerto 8080")
+
 	mux.HandleFunc("/paquetes", utils.RecibirPaquetes)
 	mux.HandleFunc("/mensaje", utils.RecibirMensaje)
 
-	panic("no implementado!")
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		panic(err)
